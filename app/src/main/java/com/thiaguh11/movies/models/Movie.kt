2054f825a5a91c013b60c62f44c082f52e78a3b8
thiaguh11 <44513705+thiaguh11@users.noreply.github.com>
2020-02-19 11:46:42 -0300

    Creating Movie Model

---
+++ app/src/main/java/com/thiaguh11/movies/models/Movie.kt
@@ -0,0 +1,15 @@
+package com.thiaguh11.movies.models
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(
+    val title: String,
+    val overview: String,
+    @Json(name = "release_date")
+    val releaseDate: String,
+    @Json(name = "poster_path")
+    val imageUrl: String
+) : Parcelable
