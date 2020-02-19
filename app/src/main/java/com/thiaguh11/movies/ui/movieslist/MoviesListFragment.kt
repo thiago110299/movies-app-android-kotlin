8d725041dfb838f6e0bf4208ad29024651600fd0
thiaguh11 <44513705+thiaguh11@users.noreply.github.com>
2020-02-19 12:26:58 -0300

    Update Movies List Fragment

--- app/src/main/java/com/thiaguh11/movies/ui/movieslist/MoviesListFragment.kt
+++ app/src/main/java/com/thiaguh11/movies/ui/movieslist/MoviesListFragment.kt
@@ -6,20 +6,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 
 import com.thiaguh11.movies.R
+import com.thiaguh11.movies.databinding.FragmentMoviesListBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class MoviesListFragment : Fragment() {
 
+    private val viewModel: MoviesListViewModel by lazy {
+        ViewModelProviders.of(this).get(MoviesListViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+        val binding = FragmentMoviesListBinding.inflate(inflater)
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+
+        binding.recyclerView.adapter = MoviesAdapter(MoviesAdapter.OnClickListener{
+            viewModel.displayMovieDetails(it)
+        })
+
+        viewModel.navigateToSelectedMovie.observe(this, Observer { movie ->
+            if(movie != null) {
+                findNavController().navigate(MoviesListFragmentDirections.actionMoviesListFragmentToMoviesDetailsFragment(movie))
+                viewModel.displayMovieDetailsComplete()
+            }
+        })
+
+        return binding.root
     }
 
 
