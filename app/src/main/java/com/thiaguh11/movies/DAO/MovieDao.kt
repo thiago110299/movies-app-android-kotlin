404a34cdf654f8d2432b1eabfdfc151a7f1d69b6
thiaguh11 <44513705+thiaguh11@users.noreply.github.com>
2020-02-20 12:25:41 -0300

    Creating MovieDao

---
+++ app/src/main/java/com/thiaguh11/movies/DAO/MovieDao.kt
@@ -0,0 +1,17 @@
+package com.thiaguh11.movies.DAO
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.thiaguh11.movies.database.DatabaseMovie
+
+@Dao
+interface MovieDao {
+    @Query("select * from databasemovie")
+    fun getMovies(): LiveData<List<DatabaseMovie>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(videos: List<DatabaseMovie>)
+}
