a16b013b43205efcfbb8b91e29c24d0febf200a0
thiaguh11 <44513705+thiaguh11@users.noreply.github.com>
2020-02-21 11:43:21 -0300

    Update Package ui and util

--- app/src/main/java/com/thiaguh11/movies/ui/MainActivity.kt
+++ app/src/main/java/com/thiaguh11/movies/ui/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.thiaguh11.movies
+package com.thiaguh11.movies.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.thiaguh11.movies.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
