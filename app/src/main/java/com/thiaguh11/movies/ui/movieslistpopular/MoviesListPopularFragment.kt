d234cdab681d50c70af08feeb0ca1d6abb96f188
thiaguh11 <44513705+thiaguh11@users.noreply.github.com>
2020-02-25 23:10:50 -0300

    Update MoviesListPopularFragment

--- app/src/main/java/com/thiaguh11/movies/ui/movieslistpopular/MoviesListPopularFragment.kt
+++ app/src/main/java/com/thiaguh11/movies/ui/movieslistpopular/MoviesListPopularFragment.kt
@@ -1,12 +1,17 @@
 package com.thiaguh11.movies.ui.movieslistpopular
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import com.thiaguh11.movies.R
 import com.thiaguh11.movies.databinding.FragmentMoviesListBinding
+import com.thiaguh11.movies.ui.adapters.MoviesListPopularAdapter
 
 class MoviesListPopularFragment : Fragment() {
 
@@ -25,11 +30,12 @@ class MoviesListPopularFragment : Fragment() {
 
         binding.viewModel = popularViewModel
 
-        binding.recyclerView.adapter = MoviesAdapter(MoviesAdapter.OnClickListener{ movie ->
-            popularViewModel.displayMovieDetails(movie)
-        })
+        binding.recyclerView.adapter =
+            MoviesListPopularAdapter(MoviesListPopularAdapter.OnClickListener { movie ->
+                popularViewModel.displayMovieDetails(movie)
+            })
 
-        popularViewModel.navigateToSelectedMovie.observe(this, Observer { selectedMovie ->
+        popularViewModel.navigateToSelectedMovie.observe(viewLifecycleOwner, Observer { selectedMovie ->
             selectedMovie?.let { movie ->
                 findNavController().navigate(MoviesListPopularFragmentDirections.actionMoviesListFragmentToMoviesDetailsFragment(movie))
                 popularViewModel.displayMovieDetailsComplete()
@@ -39,5 +45,36 @@ class MoviesListPopularFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+
+        val manager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu.findItem(R.id.search)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(activity?.componentName))
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery("", false)
+                searchItem.collapseActionView()
+
+                query?.let { queryMovie ->
+                    findNavController().navigate(MoviesListPopularFragmentDirections.actionMoviesListFragmentToSearchResultsFragment(queryMovie))
+                }
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu, inflater)
     }
 }
