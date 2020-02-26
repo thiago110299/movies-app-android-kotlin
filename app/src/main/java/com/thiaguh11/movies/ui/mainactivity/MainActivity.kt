ce09bd9cfd5a9baca9f1db171ead36993980ed5c
thiaguh11 <44513705+thiaguh11@users.noreply.github.com>
2020-02-25 23:07:47 -0300

    Change Main Activity

--- app/src/main/java/com/thiaguh11/movies/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/thiaguh11/movies/ui/mainactivity/MainActivity.kt
@@ -1,45 +1,20 @@
 package com.thiaguh11.movies.ui.mainactivity
 
-import android.app.SearchManager
-import android.content.Context
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.thiaguh11.movies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.myNavHostFragment))
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-
-        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val searchItem = menu?.findItem(R.id.search)
-        val searchView = searchItem?.actionView as SearchView
-
-        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
-
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                searchView.clearFocus()
-                searchView.setQuery("", false)
-                searchItem.collapseActionView()
-
-
-
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-
-        })
-
-        return super.onCreateOptionsMenu(menu)
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.myNavHostFragment).navigateUp()
     }
 }
